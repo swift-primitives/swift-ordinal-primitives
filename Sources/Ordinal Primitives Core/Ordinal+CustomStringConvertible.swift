8399926b2a8f852c613a35109d52647b5576eea9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-02-12 22:07:46 +0100

    Add Ordinal CustomStringConvertible conformance

---
+++ Sources/Ordinal Primitives Core/Ordinal+CustomStringConvertible.swift
@@ -0,0 +1,14 @@
+// ===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-primitives open source project
+//
+// Copyright (c) 2024-2026 Coen ten Thije Boonkkamp and the swift-primitives project authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE for license information
+//
+// ===----------------------------------------------------------------------===//
+
+extension Ordinal: CustomStringConvertible {
+    public var description: String { rawValue.description }
+}
