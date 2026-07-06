bd002b2218c6f4dcaed9d3fbaf6e6fa0bdce621a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-07-07 01:52:21 +0200

    style: sort import statements lexicographically (swift-format OrderedImports)

--- Sources/Ordinal Primitives Standard Library Integration/exports.swift
+++ Sources/Ordinal Primitives Standard Library Integration/exports.swift
@@ -1,8 +1,8 @@
 // exports.swift
 // Re-export dependencies for SLI consumers.
 
-@_exported public import Ordinal_Primitive
 @_exported public import Ordinal_Error_Primitives
+@_exported public import Ordinal_Primitive
 @_exported public import Ordinal_Protocol_Primitives
 @_exported public import Ordinal_Tagged_Primitives
 @_exported public import Tagged_Primitives_Standard_Library_Integration
